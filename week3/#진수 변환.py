4b685020739e20bf51a25577b1622bc4d2fff519
euyju <x5459156@gmail.com>
2025-03-26 22:59:12 +0900

    week3 script1

--- week3/#진수 변환.py
+++ week3/#진수 변환.py
@@ -14,9 +14,6 @@ elif sel == 10 :
     num10 = int(num)
 elif sel == 2 :
     num10 = int(num,2)
-else : 
-    print("2, 8, 10, 16 중 하나만 입력하세요. 프로그램을 종료합니다.")
-    exit() 
 
 
 print("16진수==>",hex(num10))
