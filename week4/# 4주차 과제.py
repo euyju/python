71c941e7477741b4d16820970f4718f85a547dd7
euyju <x5459156@gmail.com>
2025-03-28 13:59:00 +0900

    4주차 과제

---
+++ week4/# 4주차 과제.py
@@ -0,0 +1,17 @@
+# 과제
+
+cal = int(input("1.입력한 수식 계산 2. 두수 사이의 합계 :"))
+def sum_between(a, b):
+    return sum(range(min(a, b), max(a, b) + 1))
+
+if cal == 1 :
+    math = input("수식을 입력하세요:")
+    result = eval(math)
+    print(math,"결과는 ",result," 입니다.")
+else :
+    x = int(input("첫 번째 숫자를 입력하세요 :"))
+    y = int(input("두 번째 숫자를 입력하세요"))
+
+    z= sum_between(x,y)
+
+    print(x,"+...+",y,"는",z,"입니다.")
---
+++ week4/Untitled-1.py
@@ -0,0 +1,12 @@
+number = int(input("정수 입력>"))
+
+#양수 조건
+if number > 0:
+    print("양수입니다")
+
+if number < 0:
+    print("음수입니다")
+
+# 0 조건
+if number == 0:
+    print("0입니다")
